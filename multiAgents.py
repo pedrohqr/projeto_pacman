e2f8fa52b81cc614ab2d613f22083806f329a22d
pedrohqr <pedrohq.r@hotmail.com>
2022-04-30 18:19:01 -0400

    Initial commit

---
+++ multiAgents.py
@@ -0,0 +1,161 @@
+# multiAgents.py
+# --------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+from util import manhattanDistance
+from game import Directions
+import random, util
+
+from game import Agent
+
+def scoreEvaluationFunction(currentGameState):
+    """
+    This default evaluation function just returns the score of the state.
+    The score is the same one displayed in the Pacman GUI.
+
+    This evaluation function is meant for use with adversarial search agents
+    (not reflex agents).
+    """
+    return currentGameState.getScore()
+
+class MultiAgentSearchAgent(Agent):
+    """
+    This class provides some common elements to all of your
+    multi-agent searchers.  Any methods defined here will be available
+    to the MinimaxPacmanAgent, AlphaBetaPacmanAgent & ExpectimaxPacmanAgent.
+
+    You *do not* need to make any changes here, but you can if you want to
+    add functionality to all your adversarial search agents.  Please do not
+    remove anything, however.
+
+    Note: this is an abstract class: one that should not be instantiated.  It's
+    only partially specified, and designed to be extended.  Agent (game.py)
+    is another abstract class.
+    """
+
+    def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
+        self.index = 0 # Pacman is always agent index 0
+        self.evaluationFunction = util.lookup(evalFn, globals())
+        self.depth = int(depth)
+
+class MinimaxAgent(MultiAgentSearchAgent):
+   
+    def minimizer(self, game_state, depth, agent):
+        actions = game_state.getLegalActions(agent)
+        scores = []
+
+        for action in actions:
+            successor_game_state = game_state.generateSuccessor(agent, action)
+
+            if agent == game_state.getNumAgents() - 1:  # last ghost
+                scores.append(self.minimax(successor_game_state, depth - 1, agent=0, maximizing=True)[0])
+            else:
+                scores.append(self.minimax(successor_game_state, depth, agent=agent + 1, maximizing=False)[0])
+
+        min_score = min(scores)
+        min_indexes = [i for i, score in enumerate(scores) if score == min_score]
+        chosen_action = actions[random.choice(min_indexes)]
+
+        return min_score, chosen_action
+
+    def maximizer(self, game_state, depth, agent):
+        actions = game_state.getLegalActions(agent)
+        scores = []
+
+        for action in actions:
+            successor_game_state = game_state.generateSuccessor(agent, action)
+            scores.append(self.minimax(successor_game_state, depth, agent=agent + 1, maximizing=False)[0])
+
+        max_score = max(scores)
+        max_indexes = [i for i, score in enumerate(scores) if score == max_score]
+        chosen_action = actions[random.choice(max_indexes)]
+
+        return max_score, chosen_action
+
+    def minimax(self, game_state, depth, agent=0, maximizing=True):
+        if depth == 0 or game_state.isWin() or game_state.isLose():
+            return self.evaluationFunction(game_state), Directions.STOP
+
+        if maximizing:
+            return self.maximizer(game_state, depth, agent)
+        else:
+            return self.minimizer(game_state, depth, agent)
+
+    def getAction(self, gameState):
+        """
+        Returns the minimax action from the current gameState using self.depth
+        and self.evaluationFunction.
+
+        Here are some method calls that might be useful when implementing minimax.
+
+        gameState.getLegalActions(agentIndex):
+        Returns a list of legal actions for an agent
+        agentIndex=0 means Pacman, ghosts are >= 1
+
+        gameState.getNextState(agentIndex, action):
+        Returns the child game state after an agent takes an action
+
+        gameState.getNumAgents():
+        Returns the total number of agents in the game
+
+        gameState.isWin():
+        Returns whether or not the game state is a winning state
+
+        gameState.isLose():
+        Returns whether or not the game state is a losing state
+        """
+        "*** YOUR CODE HERE ***"
+        chosen_action = self.minimax(gameState, self.depth)[1]
+        return chosen_action
+        
+        util.raiseNotDefined()
+
+class AlphaBetaAgent(MultiAgentSearchAgent):
+    """
+    Your minimax agent with alpha-beta pruning (question 3)
+    """
+
+    def getAction(self, gameState):
+        """
+        Returns the minimax action using self.depth and self.evaluationFunction
+        """
+        "*** YOUR CODE HERE ***"
+        util.raiseNotDefined()
+
+class ExpectimaxAgent(MultiAgentSearchAgent):
+    """
+      Your expectimax agent (question 4)
+    """
+
+    def getAction(self, gameState):
+        """
+        Returns the expectimax action using self.depth and self.evaluationFunction
+
+        All ghosts should be modeled as choosing uniformly at random from their
+        legal moves.
+        """
+        "*** YOUR CODE HERE ***"
+        util.raiseNotDefined()
+
+def betterEvaluationFunction(currentGameState):
+    """
+    Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
+    evaluation function (question 5).
+
+    DESCRIPTION: <write something here so we know what you did>
+    """
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()    
+
+# Abbreviation
+better = betterEvaluationFunction
